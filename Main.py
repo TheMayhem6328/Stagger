dca964f9618a506d749212e9af030317139a9a97
Zahiruzzaman Chowdhury <nebrus.gamer@gmail.com>
2022-10-14 14:35:05 +0600

    Initial Commit - Vorbis Support

---
+++ Main.py
@@ -0,0 +1,113 @@
+# Initialization
+import spotipy
+from mutagen.flac      import FLAC
+from mutagen.mp3       import MP3
+from mutagen.oggvorbis import OggVorbis
+import os
+import Stagger
+
+# Setup variable(s)
+clientID    = "2531ad4b5e3c497ca0a9fce18d1280ab"
+redirectURI = "http://127.0.0.1:8000/spotify/callback/"
+cache = spotipy.CacheFileHandler(".cache_sp")
+secret = spotipy.oauth2.SpotifyPKCE(clientID, redirectURI, cache_handler=cache, scope=["user-library-read"])
+
+# Define root class
+
+# Change cwd to the folder where this script is located
+workDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(workDir)
+
+try:
+    fileList = os.listdir(".//Audio")
+    # fileList = ["Track.flac"] # [x for x in fileList2 if re.search("mp3",x)]
+except FileNotFoundError:
+    os.mkdir(".//Audio")
+    print("\nPut audio files the folder \"Audio\" to scan them\n")
+    exit()
+
+count, success, fail = 1, 0, 0
+for filename in fileList:
+    print("< ========================================\n")
+
+    # Find track and type
+    print(f"Item {count}\n--------------\nFile name: {filename}")
+    file = Stagger.findTypeFunc(".//Audio//"+filename)
+
+    # If file is supported
+    if file != "UNSUPPORTED":
+        # Assign variables in relation to file type
+        if   type(file) == FLAC or type(file) == OggVorbis:
+            tag = Stagger.tag.vorbis
+        elif type(file) == MP3:
+            tag = Stagger.tag.id3
+
+        titleTag       = tag[0]
+        albumTag       = tag[3]
+        albumArtistTag = tag[6]
+
+        # Build Query
+
+        ## Check if title is already present - if not, ask for input
+        try:
+            trackTitleSearch  = file[titleTag][0]
+            print(f"Track title: {trackTitleSearch}")
+        except KeyError:
+            trackTitleSearch = input("Track title not found - enter manually: ")
+
+        ## Check if album name is already present - if not, ask for input
+        try:
+            trackAlbumSearch = file[albumTag][0]
+            print(f"Album: {trackAlbumSearch}")
+        except KeyError:
+            trackAlbumSearch = input("Album name not found - enter manually: ")
+
+        ## Check if album artist is already present - if not, ask for input
+        try:
+            trackArtistSearch = file[albumArtistTag][0]
+            print(f"Album artist: {trackArtistSearch}")
+        except KeyError:
+            trackArtistSearch = input("Album artist not found - enter manually: ")
+
+        # Retrieve and write data
+        print("\n")
+        try:
+            x = 0
+            while True:
+                ## Accumulate data
+                trackData = Stagger.trackMeta(trackTitleSearch+" "+trackArtistSearch,secret, x, tag)
+
+                ## Verify data
+                if (
+                    trackTitleSearch  == trackData[titleTag][0] and
+                    trackAlbumSearch  == trackData[albumTag][0] and
+                    trackArtistSearch == trackData[albumArtistTag][0]
+                ):
+                    ### Output data to STDOUT
+                    finalTrackData = {}
+                    for x in list(trackData.items()):
+                        print(str(x[0]) + ": " + str(x[1]))\
+
+
+                    ### Save data to file and exit while loop
+                    file.update(trackData)
+                    file.save()
+                    print(f"\nSaved file {filename}")
+                    success += 1
+                    break
+
+                else: ### If current search item didn't match existing, report
+                    print(f"Search item {str(x+1).zfill(2)} doesn't match")
+                    x += 1
+        except (IndexError, TypeError): ### If data was not found on Spotify, report
+            print(f"\nMetadata for file {filename} not found on Spotify - maybe check its title, album and album artist tags?")
+            fail += 1
+
+    print("\n======================================== >\n\n\n")
+    count += 1
+
+# A small detail thing
+def countPrint(count: int): return f'{count} files' if count != 1 else f'{count} file'
+
+# Print summary
+print(f"Summary:\n{countPrint(success)} updated, {countPrint(fail)} failed, {countPrint(count - (success + fail))} ignored")
---
+++ Stagger.py
@@ -0,0 +1,235 @@
+"""This class contains all methods and data for Stagger. Feel free to reuse it :3"""
+
+# Initialization
+import spotipy
+import mutagen
+from mutagen           import id3
+from mutagen.flac      import FLAC
+from mutagen.mp3       import MP3
+from mutagen.oggvorbis import OggVorbis
+
+# Define class containing tag list
+class tag:
+    """Data values (list): `vorbis`, `id3`"""
+
+    vorbis = [
+        "title",
+        "track",
+        "tracktotal",
+        "album",
+        "year",
+        "date",
+        "albumartist",
+        "discnumber",
+        "disctotal",
+        "bpm",
+        "isrc",
+        "upc",
+        "spotifyTrackID",
+        "spotifyAlbumID",
+        "artists",
+        "releasetype",
+        "initialkey"
+    ]
+
+    id3 = [
+        "TIT2",
+        "TRCK",
+        "TRKN",
+        "TALB",
+        "TDRC",
+        "TDAT",
+        "TPE2",
+        "TPOS",
+        "DISK",
+        "TBPM",
+        "TSRC",
+        "TXXX:BARCODE",
+        "TXXX:SPOTIFYTRACKID",
+        "TXXX:SPOTIFYALBUMID",
+        "TPE1",
+        "TXXX:MusicBrainz Album Type",
+        "TKEY"
+    ]
+
+# Find metadata in Spotify
+def trackMeta(query: str, auth_mgr: spotipy.SpotifyOAuth, index: int = 0, nameList: list = None):
+    """## Find metadata of `{query}` from spotify
+
+    ### Args:
+    - query (str): String to search on Spotify
+    - auth_mgr (spotipy.SpotifyOAuth): Spotipy OAuthManager object
+    - index (int, optional): Check search item `{index}`. Defaults to 0.
+    - nameList (list):  Contains a list of tag names orgainzed in a specific order.
+                        See `idList` in this function to see format.
+                        Defaults to a set meant for vorbis tags (inside function).
+
+    ### Returns:
+    - dict: Contains search result of `{query}`
+    """
+
+    # Initialize
+    spotify = spotipy.Spotify(auth_manager=auth_mgr)
+    idList  = tag.vorbis if nameList == None else nameList
+
+    # Build query
+    try:
+        result            = spotify.search(q=query,type="track")
+    except KeyboardInterrupt:
+        print("\Operation terminated by user")
+        print("\n======================================== >")
+        exit()
+    except:
+        print("Network error - try again when you have a working internet connection")
+        print("\n======================================== >")
+        exit()
+    resultTrack       = spotify.track(track_id=result["tracks"]["items"][index]["id"])
+    resultAlbum       = spotify.album(album_id=resultTrack["album"]["id"])
+    resultFeatures    = spotify.audio_features(resultTrack["id"])[0]
+
+    # Build dict
+    trackMeta = {}
+    def trackAdd(key: str ,data: list): trackMeta.update({key : data})
+
+
+    # Add simple ones
+    trackAdd(idList[0] ,[resultTrack["name"]])
+    trackAdd(idList[1] ,[str(resultTrack["track_number"])])
+    trackAdd(idList[2] ,[str(resultTrack["album"]["total_tracks"])])
+    trackAdd(idList[3] ,[resultTrack["album"]["name"]])
+    trackAdd(idList[4] ,[resultAlbum["release_date"][0:4]])
+    trackAdd(idList[5] ,[resultAlbum["release_date"]])
+    trackAdd(idList[6] ,[resultTrack["album"]["artists"][0]["name"]])
+    trackAdd(idList[7] ,[str(resultTrack["disc_number"])])
+    trackAdd(idList[8] ,[str(resultAlbum["tracks"]["items"][len(resultAlbum["tracks"]["items"]) - 1]["disc_number"])])
+    trackAdd(idList[9] ,[str(round(resultFeatures["tempo"]))])
+    trackAdd(idList[10],[resultTrack["external_ids"]["isrc"]])
+    trackAdd(idList[11],[resultAlbum["external_ids"]["upc"]])
+    trackAdd(idList[12],[resultTrack["id"]])
+    trackAdd(idList[13],[resultTrack["album"]["id"]])
+
+    # Do some manupilation to add a bit more complicated ones
+
+    ## Artist
+    artist  = [str(resultTrack["artists"][0]["name"])]
+    length = len(resultTrack["artists"])
+    if length > 1:
+        for x in range(1, length):
+            artist = artist + [resultTrack["artists"][x]["name"]]
+    trackAdd(idList[14],artist)
+
+    ## Release type
+    trackType = str(resultTrack["album"]["album_type"])
+    if trackType == "single":
+        trackType = ["Single or EP"]
+    else:
+        trackType = [trackType.capitalize()]
+    trackAdd(idList[15],trackType)
+
+    ## Key
+
+    ### Map numbers to classical key
+    keyMap = {
+        "-1" : "",
+        "0"  : "C",
+        "1"  : "Db",
+        "2"  : "D",
+        "3"  : "Eb",
+        "4"  : "E",
+        "5"  : "F",
+        "6"  : "Gb",
+        "7"  : "G",
+        "8"  : "Gb",
+        "9"  : "A",
+        "10" : "Ab",
+        "11" : "B"
+    }
+
+    ### Account for musical mode (Minor or Major)
+    modeMap = ["m",""]
+
+    ### Find classical key
+    key = str(resultFeatures["key"])
+    trackKey = keyMap[key]+modeMap[resultFeatures["mode"]]
+
+    ### Map classical key to camelot
+    camelotMap = {
+        "C"   : "8B",
+        "Db"  : "3B",
+        "D"   : "10B",
+        "Eb"  : "5B",
+        "E"   : "12B",
+        "F"   : "7B",
+        "Gb"  : "2B",
+        "G "  : "9B",
+        "Ab"  : "4B",
+        "A"   : "11B",
+        "Bb"  : "6B",
+        "B "  : "1B",
+        "Cm"  : "5A",
+        "Dbm" : "12A",
+        "Dm"  : "7A",
+        "Ebm" : "2A",
+        "Em"  : "9A",
+        "Fm"  : "4A",
+        "Gbm" : "11A",
+        "Gm"  : "6A",
+        "Abm" : "1A",
+        "Am"  : "8A",
+        "Bbm" : "3A",
+        "Bm"  : "10A"
+    }
+
+    ## Explicitness
+    if resultTrack["explicit"]:
+        trackAdd("itunesadvisory" ,['1'])
+    elif not resultTrack["explicit"]:
+        trackAdd("itunesadvisory" ,['0'])
+
+    # Append to dictionary
+    trackAdd(idList[16],[str(trackKey)]+[str(camelotMap[trackKey])])
+
+    # Return Dictionary
+    return trackMeta
+
+# Check audio encoding type and return it
+def findTypeFunc(audioFileName: str):
+    """## Returns function for filetype or literal "UNSUPPORTED" if filetype not defined
+
+    ### Args:
+    - audioFileName (str): Name of the file we need to check
+
+    ### Returns (One Of):
+    - function:      Returns function compatible with given file name
+    - "UNSUPPORTED": Returns this literal only if no compatible function for `{audioFileName}` was found
+    """
+
+    try:
+        try:
+            filetype = type(mutagen.File(audioFileName))
+        except:
+            raise UnboundLocalError
+        if filetype == FLAC:
+            fileFunc = FLAC(audioFileName)
+            print("Type: FLAC")
+        if filetype == OggVorbis:
+            fileFunc = OggVorbis(audioFileName)
+            print("Type: OGG Vorbis")
+        elif filetype == MP3:
+            fileFunc = MP3(audioFileName)
+            print("Type: MP3")
+            raise UnboundLocalError
+        if fileFunc != None:
+            return fileFunc
+        else:
+            return "UNSUPPORTED"
+    except UnboundLocalError:
+        print("Type: Undefined / Non-audio")
+        return "UNSUPPORTED"
+
+# Define a function to make it simpler to add tags
+def addTag(tagName : str, tagData : list, file):
+    if type(file) == MP3:
+        file.update({tagName: id3.TextFrame(encoding=3, text=tagData )})
+    else:
+        file.update({tagName: tagData})
